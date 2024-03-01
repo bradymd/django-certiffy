06f5d9d8ac99fcd80f05bbd76d7d552e5b2de70b
Mark Brady <m.brady@herts.ac.uk>
2024-03-01 17:10:50 +0000

    new file:   deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+git clone git@github.com:bradymd/django-certiffy.git
+cd django-certiffy
+python3 -m venv venv && source venv/bin/activate
+cd django_certiffy_project
+python3 -m pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+rm db.sqlite3 
+python manage.py migrate --fake-initial
+python manage.py migrate --run-syncdb
+DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_PASSWORD=psw  python manage.py createsuperuser --email=admin@example.com --noinput
+python manage.py runserver 
