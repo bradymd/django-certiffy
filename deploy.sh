9b765fb92a03bd5707aa18ad3aa6401942b90ce2
Mark Brady <bradymd@users.noreply.github.com>
2024-03-22 13:48:48 +0000

    Update deploy.sh
    Just being consistent in use of python vs python3

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ cd django_certiffy_project
 python3 -m pip install -r requirements.txt
 python3 -m pip install --upgrade pip
 rm db.sqlite3 
-python manage.py migrate --fake-initial
-python manage.py migrate --run-syncdb
+python3 manage.py migrate --fake-initial
+python3 manage.py migrate --run-syncdb
 DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_PASSWORD=psw  python manage.py createsuperuser --email=admin@example.com --noinput
-python manage.py runserver 
+python3 manage.py runserver 
